37fc6564f1f18e9c640f08d8aa944fb8f61dced4
Shivang <harshsaini5656@gmail.com>
2024-02-23 11:34:15 +0530

    Added rersolver frontend

---
+++ resources/js/components/Resolver/IssueCard.vue
@@ -0,0 +1,74 @@
+<template>
+  <div class="card-main-div shadow-2xl px-4 py-3">
+    <div class="card-div-header">
+      <img
+        src="https://qph.fs.quoracdn.net/main-qimg-2b21b9dd05c757fe30231fac65b504dd"
+        alt="none"
+      />
+      <p class="mr-5">Rahul Singh</p>
+    </div>
+    <h1>Title of the issue</h1>
+    <p>
+      Lorem ipsum dolor sit amet consectetur, adipisicing elit. Soluta harum
+      vitae recusandae aut est cupiditate suscipit accusamus nemo eum
+      dignissimos, corporis veniam quis minima repudiandae!
+    </p>
+    <div class="card-button">
+      <button
+        class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 mr-4 rounded"
+        @click="feedback = !feedback"
+      >
+        {{ feedback ? "Close" : "Open" }}
+      </button>
+    </div>
+    <div v-if="feedback" class="feedback">
+      <textarea style="width: 100%" v-model="text"></textarea>
+      <button
+        disabled="!text"
+        class="bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded mr-4"
+      >
+        Resolve
+      </button>
+      <button
+        disabled="!text"
+        class="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded"
+      >
+        Reject
+      </button>
+    </div>
+  </div>
+</template>
+<script setup>
+import { ref } from "vue";
+const text = ref("");
+const feedback = ref(false);
+</script>
+<style scoped>
+.card-main-div {
+  min-height: 15vmax;
+  transition: all 0.5s;
+}
+.card-main-div:hover {
+  transform: scale(1.05);
+}
+.card-div-header {
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  margin-bottom: 4px;
+}
+.card-div-header > img {
+  width: 4vmax;
+  border-radius: 50%;
+}
+.card-button {
+  display: flex;
+}
+.feedback {
+  width: 100%;
+  margin-top: 1vmax;
+}
+textarea:focus {
+  outline: none;
+}
+</style>
---
+++ resources/js/components/Resolver/MainPage.vue
@@ -0,0 +1,67 @@
+<template>
+  <div class="main-div">
+    <div class="left-div">
+      <img
+        src="https://qph.fs.quoracdn.net/main-qimg-2b21b9dd05c757fe30231fac65b504dd"
+        alt="resolver"
+      />
+      <p>Shivam Teotia</p>
+      <button
+        class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mt-4"
+      >
+        <router-link to="/resolver-profile"> My Profile </router-link>
+      </button>
+    </div>
+    <div class="right-div">
+      <p>Your Department Related Issues</p>
+      <div v-for="item in 5" :key="item" class="right-div-card">
+        <IssueCard />
+      </div>
+    </div>
+  </div>
+</template>
+<script setup>
+import IssueCard from "./IssueCard.vue";
+</script>
+<style scoped>
+.main-div {
+  height: 100vh;
+  display: flex;
+  gap: 2vmax;
+  width: 100%;
+}
+.left-div {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  width: 25%;
+}
+.left-div > img {
+  margin-top: 2vmax;
+  width: 80%;
+  border-radius: 50%;
+  margin-bottom: 2vmax;
+}
+.left-div > p {
+  font-family: "Segoe UI", Tahoma, Geneva, Verdana, sans-serif;
+  font-weight: 700;
+}
+.right-div {
+  width: 75%;
+  background-color: rgb(248, 243, 243);
+  overflow-y: auto;
+  border-radius: 5px;
+}
+.right-div > p {
+  margin: 5px 0;
+  text-align: center;
+  font-size: 2vmax;
+  color: gray;
+  padding-bottom: 1.2vmax;
+  font-family: Verdana, Geneva, Tahoma, sans-serif;
+  border-bottom: 1px solid grey;
+}
+.right-div-card {
+  padding: 1rem 2rem;
+}
+</style>
