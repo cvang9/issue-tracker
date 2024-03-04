ebdfa6b55aeda69b57208c490a05bcaf6edc86ed
Shivam-Teotia <shivamteotia200@gamil.com>
2024-03-04 10:43:56 +0530

    adding logout

--- resources/js/components/Resolver/Resolver.vue
+++ resources/js/components/Resolver/Resolver.vue
@@ -131,6 +131,27 @@
           </p>
         </div>
       </router-link>
+      <div class="flex text-white justify-center space-x-3 bg-red w-full mt-5 h-full">
+        <div>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke-width="1.5"
+            stroke="currentColor"
+            class="h-8 w-8 text-white-500"
+            width="24"
+            height="24"
+          >
+            <path
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              d="M8.25 9V5.25A2.25 2.25 0 0 1 10.5 3h6a2.25 2.25 0 0 1 2.25 2.25v13.5A2.25 2.25 0 0 1 16.5 21h-6a2.25 2.25 0 0 1-2.25-2.25V15m-3 0-3-3m0 0 3-3m-3 3H15"
+            />
+          </svg>
+        </div>
+        <div class="h-8 w-8 mt-1 text-lg">LogOut</div>
+      </div>
     </section>
     <PageLoader v-if="loadingTickets" />
     <section
