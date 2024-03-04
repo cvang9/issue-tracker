6c0c083fe7fb10c41d80bd57a465781e92772d87
Shivang <harshsaini5656@gmail.com>
2024-03-04 22:20:12 +0530

    bug-fix(3)

--- resources/js/components/IntroBox.vue
+++ resources/js/components/IntroBox.vue
@@ -2,8 +2,8 @@
     <div class="login-box">
         <div class="login-box-quotebox">
         <div class="quote-container">
-            <h1 style="color: white;">Hi</h1>
-            <h2>Welcome to</h2>
+            <!-- <h1 style="color: white;">Hi</h1>
+            <h2>Welcome to</h2> -->
 
             <div class="quote" style="font-size: 90px;">Issue Tracker System</div>
             
